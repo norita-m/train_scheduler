1d27d0193ee33945246801c4c6030dbba213c2ac
Nora Mata <norita@Noras-MacBook-Pro.local>
2016-12-10 17:15:26 -0600

    updated html file

---
+++ assets/logic.js
@@ -0,0 +1,12 @@
+
+ 
+
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAGk4I0VgLiSACV90Hf0-EmBdMCCV90J6Q",
+    authDomain: "trains-ccc3f.firebaseapp.com",
+    databaseURL: "https://trains-ccc3f.firebaseio.com",
+    storageBucket: "trains-ccc3f.appspot.com",
+    messagingSenderId: "1057439072596"
+  };
+  firebase.initializeApp(config);
