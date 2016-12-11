2195eeaf8f3f9bd8274d7e110e26c2973cb83365
Nora Mata <norita@Noras-MacBook-Pro.local>
2016-12-10 18:44:16 -0600

    html finished, beginning js

--- assets/logic.js
+++ assets/logic.js
@@ -1,6 +1,3 @@
-
- 
-
  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyAGk4I0VgLiSACV90Hf0-EmBdMCCV90J6Q",
@@ -10,3 +7,34 @@
     messagingSenderId: "1057439072596"
   };
   firebase.initializeApp(config);
+
+//VARIABLES
+  var database = firebase.database();
+  var trainName;
+  var destination;
+  var trainTime;
+  var frequency;
+
+//FUNCTION
+//when the user clicks submit, this function will run
+$("submit").on("click", function(event) {
+
+	//when user enters train name, this text is rendered in corresponding ID's div
+	//val and trim prevent spaces from rendering
+	trainName = $("#trainName").val().trim();
+	destination = $("#destination").val().trim();
+	trainTime = $("#trainTime").val().trim();
+	frequency = $("#frequency").val().trim();
+
+	//QUESTION: this pushes the information to the database (why are the parenthesis empty??)
+	database.ref().push({
+	trainName: trainName,
+	destination: destination, 
+	trainTime: trainTime,
+	frequency: frequency,
+
+	});
+
+	return false;
+
+});
