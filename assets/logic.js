63ccbfd761f0efa8f0bd9f02a604ac511e1309a7
Nora Mata <norita@Noras-MacBook-Pro.local>
2016-12-14 17:38:59 -0600

    uploading homework

--- assets/logic.js
+++ assets/logic.js
@@ -17,7 +17,7 @@
 
 //FUNCTION
 //when the user clicks submit, this function will run
-$("submit").on("click", function(event) {
+$("#submit").on("click", function(event) {
 
 	//when user enters train name, this text is rendered in corresponding ID's div
 	//val and trim prevent spaces from rendering
@@ -26,15 +26,67 @@ $("submit").on("click", function(event) {
 	trainTime = $("#trainTime").val().trim();
 	frequency = $("#frequency").val().trim();
 
-	//QUESTION: this pushes the information to the database (why are the parenthesis empty??)
+	//console logs
+	console.log(trainName);
+	console.log(destination);
+	console.log(trainTime);
+	console.log(frequency);
+
+	//this pushes the information to the database 
 	database.ref().push({
-	trainName: trainName,
-	destination: destination, 
-	trainTime: trainTime,
-	frequency: frequency,
+		trainName: trainName,
+		destination: destination, 
+		trainTime: trainTime,
+		frequency: frequency,
 
 	});
 
 	return false;
 
 });
+
+//this calls firebase
+database.ref().on("child_added", function(snap) {
+	
+	// //this will calculate the difference of the next arrival
+	var firstTrainInput = snap.val().trainTime;
+	console.log("First Train = " + firstTrainInput);
+	// //moment - convert time 
+	var timeConverted = moment(trainTime, "HHmm");
+	console.log("time converted = " + timeConverted);
+
+	//calculate arrival of next train (firstTrain + Next Frequency)
+	//NOT RIGHT var nextTrain = moment().diff(timeConverted, "hh:mm");
+	//console.log("Next Train = " + nextTrain);
+
+	//NEED FORMULA TO CALCULATE HOW MANY MINUTES AWAY
+
+	var timeFromStart = moment().add(timeConverted, "minutes");
+	console.log("Total time from Train Start = " + timeFromStart);
+
+	var minTillNext = (timeFromStart % snap.val().frequency);
+	console.log("Next train arriving in = " + minTillNext.toString() + "minutes");
+
+	var newRow = $("<tr>");
+	newRow.append($("<td>" + snap.val().trainName + "</td>"));
+	newRow.append($("<td>" + snap.val().destination + "</td>"));
+	newRow.append($("<td>" + snap.val().frequency + "</td>"));
+	newRow.append($("<td>" + snap.val().nextTrain + "</td>"));
+	//newRow.append($("<td>" + snap.val().minutesAway + "</td>"));
+	 $("tbody").append(newRow);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
